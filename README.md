6531fd4000a45af69d332fd0dd8e66265ae1aa0e
chutchai.kp <chutchai.kp@gmail.com>
2021-06-27 17:57:17 +0700

    Intial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# :sleeping: Easiest way to prevent Heroku Node app from Sleeping
+
+> Just create a normal JavaScript that pings the Heroku app every x minutes.
---
+++ index.js
@@ -0,0 +1,29 @@
+
+var axios = require('axios');
+
+require('dotenv').config()
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+async function Run() {
+	try {
+		var result = await axios.get(process.env.HEROKU_APP)
+		const randNumber = getRandomInt(10, 20);
+		console.log(result.data, randNumber + ' secs', new Date().toISOString())
+		setTimeout(() => {
+			Run();
+		}, randNumber * 60 * 1000);
+
+	} catch (error) {
+		console.log(error);
+		process.exit(0)
+	}
+}
+
+Run();
+
+

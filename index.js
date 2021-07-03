6493bc224612341ab2701c9a4116bd157dbf7993
chutchai.kp <chutchai.kp@gmail.com>
2021-07-03 16:26:42 +0700

    OK

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function getRandomInt(min, max) {
 async function Run() {
 	try {
 		var result = await axios.get(process.env.HEROKU_APP)
-		const randNumber = getRandomInt(10, 20);
+		const randNumber = getRandomInt(5, 10);
 		console.log(result.data, randNumber + ' minutes', new Date().toISOString())
 		setTimeout(() => {
 			Run();
